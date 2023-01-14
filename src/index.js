28c923c8c88fab55cc4a06585151b0eb2b82d197
BenZ9413 <benedikt.sell@yahoo.de>
2023-01-14 17:58:05 +0100

    added css file and corresponding webpack loader and connected it to the main file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import constructLandingpage from "./pageConstructor";
+import "./css.css";
 
 constructLandingpage();
 console.log("Wired up correctly!");
--- src/pageConstructor.js
+++ src/pageConstructor.js
@@ -1,8 +1,24 @@
+const contentNode = document.querySelector("#content");
+
 const constructLandingpage = function () {
-  const parentNode = document.querySelector("#content");
-  let childNode = document.createElement("div");
-  childNode.textContent = "It's wired up correctly!";
-  parentNode.appendChild(childNode);
+  constructHeader();
+  constructMain();
+  constructFooter();
+};
+
+const constructHeader = function () {
+  console.log("Successfully created the header!");
+  const headerContainer = document.createElement("div");
+  headerContainer.id = "headerContainer";
+  contentNode.appendChild(headerContainer);
+};
+
+const constructMain = function () {
+  console.log("Successfully created Main!");
+};
+
+const constructFooter = function () {
+  console.log("Successfully created the Footer!");
 };
 
 export default constructLandingpage;
